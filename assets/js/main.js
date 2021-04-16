8d61f7755735f079e856d580281ea74d0dc98b80
Sanjar1311 <sanjarabduxamidov1311@gmail.com>
2021-04-16 21:41:17 +0500

    add stiky navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,34 @@
 const elNav = document.querySelector('.site-header')
 const elBurger = document.querySelector('.site-header__burger')
+const elHeader = document.querySelector(".site-header")
 
 elBurger.addEventListener('click', ()=> {
   elNav.classList.toggle('nav--open')
 })
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+function onDocumentScroll(){
+  if(window.scrollY >= 200) {
+    elHeader.classList.add('site-header--fixed')
+    document.body.style.marginTop= elHeader.clientHeight + 'px'
+  } else {
+    elHeader.classList.remove('site-header--fixed')
+    document.body.style.marginTop = 0
+  }
+}
+
+window.addEventListener('scroll', debounce(onDocumentScroll, 500))
