3c1860dc4ddbf2e4449e51041b913858d9b734df
Sanjar1311 <sanjarabduxamidov1311@gmail.com>
2021-04-17 17:25:47 +0500

    change contacts sections design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,55 @@
+// const data = [
+// 	{
+// 		src: "./assets/images/stroy-control.png",
+// 		lang: ["HTML", "Scss", "Js"],
+// 		title: "Строй Контроль",
+// 		netlify: "https://stroy-kontrol-sanjar.netlify.app",
+// 		git: "https://github.com/sanjar1311/stroy-kontrol"
+// 	},
+// 	{
+// 		src: "./assets/images/tmdb.png",
+// 		lang: ["React"],
+// 		title: "TMDB movies",
+// 		netlify: "https://rmdbmoviesuz.netlify.app",
+// 		git: "https://github.com/sanjar1311/tmdbmovies"
+// 	},
+// 	{
+// 		src: "./assets/images/filter.png",
+// 		lang: ["HTML", "Scss", "Js"],
+// 		title: "Grandma",
+// 		netlify: "https://filter-project-cackes.netlify.app",
+// 		git: "https://github.com/sanjar1311/p-8--filter-project"
+// 	},
+// 	{
+// 		src: "./assets/images/myteab.png",
+// 		lang: ["HTML", "Scss", "Js"],
+// 		title: "My-team",
+// 		netlify: "https://myteam-sanjar.netlify.app",
+// 		git: "https://github.com/sanjar1311/myteam"
+// 	},
+// 	{
+// 		src: "./assets/images/dental.png",
+// 		lang: ["HTML", "Scss", "Js"],
+// 		title: "Dental Practice",
+// 		netlify: "https://dental-practice-sanjar.netlify.app",
+// 		git: "https://github.com/sanjar1311/dental-practic"
+// 	},
+// 	{
+// 		src: "./assets/images/fylo.png",
+// 		lang: ["HTML", "Scss", "Js"],
+// 		title: "Fylo",
+// 		netlify: "https://fylo-sanjar.netlify.app",
+// 		git: "https://github.com/sanjar1311/fylo"
+// 	}
+// ]
+
 const elNav = document.querySelector('.site-header')
 const elBurger = document.querySelector('.site-header__burger')
 const elHeader = document.querySelector(".site-header")
 const elNavLinks = document.querySelectorAll(".nav__link")
 const elNavList = document.querySelector(".nav__list")
+const elProjectsItemTemplate = document.querySelector('.projects-item-template')
+const elProjectsOutput = document.querySelector('.projects__list')
 
 
 // Burger Menu
@@ -11,25 +58,7 @@ elBurger.addEventListener('click', ()=> {
 })
 
 
-//  Debounce Function
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
-
-
-
-
+// Scroll
 window.addEventListener('scroll', ()=> {
 	let fromTop = window.scrollY;
 
