55937f32c8e08add4dec93777a9c63003e285c4a
Sanjar1311 <sanjarabduxamidov1311@gmail.com>
2021-04-17 11:44:57 +0500

    add novigation on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 const elNav = document.querySelector('.site-header')
 const elBurger = document.querySelector('.site-header__burger')
 const elHeader = document.querySelector(".site-header")
-const elNavLinks = document.querySelectorAll(".nav__item")
+const elNavLinks = document.querySelectorAll(".nav__link")
 const elNavList = document.querySelector(".nav__list")
 
 
@@ -28,25 +28,28 @@ function debounce(func, wait, immediate) {
 };
 
 
-//  Stiky navbar
-function onDocumentScroll(){
-  if(window.scrollY >= 200) {
-    elHeader.classList.add('site-header--fixed')
-    document.body.style.marginTop= elHeader.clientHeight + 'px'
-  } else {
-    elHeader.classList.remove('site-header--fixed')
-    document.body.style.marginTop = 0
-  }
-}
-
-window.addEventListener('scroll', debounce(onDocumentScroll, 200))
 
 
-//  Active Link
-elNavLinks.forEach(navItem => {
-	navItem.addEventListener('click', function() {
-		elNavList.querySelector('.active').classList.remove("active")
+window.addEventListener('scroll', ()=> {
+	let fromTop = window.scrollY;
 
-		navItem.classList.add('active')
+	if(window.scrollY >= 200) {
+		elHeader.classList.add('site-header--fixed')
+		document.body.style.marginTop= elHeader.clientHeight + 'px'
+	} else {
+		elHeader.classList.remove('site-header--fixed')
+		document.body.style.marginTop = 0
+	}
+
+	elNavLinks.forEach(item => {
+		const section = document.querySelector(item.hash)
+		if(
+			section.offsetTop <= fromTop &&
+			section.offsetTop + section.offsetHeight > fromTop
+		) {
+			item.classList.add('active')
+		} else {
+			item.classList.remove('active')
+		}
 	})
 })
